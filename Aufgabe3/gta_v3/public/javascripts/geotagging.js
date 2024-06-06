70a7816f8250f662826d7fd4da4b3766692eb890
Laura Butterfly <117404312+LauraButterfly@users.noreply.github.com>
2024-06-06 15:50:34 +0200

    Create geotagging.js

---
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -0,0 +1,51 @@
+// File origin: VS1LAB A2
+
+/* eslint-disable no-unused-vars */
+
+// This script is executed when the browser loads index.html.
+
+// "console.log" writes to the browser's console. 
+// The console window must be opened explicitly in the browser.
+// Try to find this output in the browser...
+console.log("The geoTagging script is going to start...");
+
+
+
+/**
+ * TODO: 'updateLocation'
+ * A function to retrieve the current location and update the page.
+ * It is called once the page has been fully loaded.
+ */
+function updateLocation() {
+    LocationHelper.findLocation((helper) => {
+        fillInCurrentLocation(helper);
+        setMapOfCurrentLocation(helper);
+    });
+}
+
+function setMapOfCurrentLocation(helper) {
+    const manager = new MapManager();
+    manager.initMap(helper.latitude, helper.longitude);
+    manager.updateMarkers(helper.latitude, helper.longitude);
+    const imageElement = document.getElementById("mapView");
+    imageElement.parentNode.removeChild(imageElement);
+    
+}
+
+function fillInCurrentLocation(helper) {
+    setValueById("longitudeTag", helper.longitude)
+    setValueById("latitudeTag", helper.latitude)
+    setValueById("longitudeDis", helper.longitude)
+    setValueById("latitudeDis", helper.latitude)
+}
+
+function setValueById(id, value) {
+    const inputElement = document.getElementById(id);
+    inputElement.value = value;
+}
+
+// Wait for the page to fully load its DOM content, then call updateLocation
+document.addEventListener("DOMContentLoaded", () => {
+    //alert("Please change the script 'geotagging.js'");
+    updateLocation();
+});
